bbc4b632a76bfba94b0e272443cdc6b5b30ebecb
dianash28 <dianash0128@gmail.com>
2021-10-12 18:54:46 -0500

    ejercicio2

--- ejercicio2.c
+++ ejercicio2.c
@@ -8,14 +8,13 @@ La función buscará el número menor dentro del arreglo,
 y regresará el número menor y la posición en donde lo encontró.
 
 Si lo encuentra, regresará número menor y su posición.
-Si no lo encontró, regresará -1.
 La función no deberá imprimir nada, solo buscar el número menor.
 
 La impresión deberá realizarse en el main.
 */
 #include <stdio.h>
 #define N 5
-//int buscarnumeromenor(int a[N], int *m);
+int buscarMenor(int arr[N], int *p);
 int main ()
 {
  int arreglo[N];
@@ -28,50 +27,30 @@ int main ()
       printf("arreglo[%d]", i);
       scanf("%d",&arreglo[i]);
   }
+  menor=buscarMenor(arreglo,&pos);
 
   
   
-   for (i=0; i<N && pos==-1; i++ )
-    {
-     if(menor>arreglo[i])
-     {
-         menor=arreglo[i];
-     }
-     
-    }
-    printf("el menor es %d en la posicion [%d] ",menor,i);
- scanf("%d[%d]", &menor,i);   
+ printf("el menor es %d en la posicion [%d] ",menor,pos);
+ scanf("%d[%d]", &menor,pos);   
 
-    
-  /*for (i=0; i<N && pos==-1; i++ )
-    {
-       printf("buscando en la posicion %d\n", i);
-       if (menor>arreglo[i])
-       {
-         menor=arreglo[i];
-         pos=i;
-       }
-    }
-    */
- 
-
-
-  //buscarnumeromenor(arreglo, & menor);
-
-  
  fflush(stdin);
 getchar();
  return 0;
 }
-/*int buscarnumeromenor(int a[N], int *m)
-{ int i;
-    for (i=0; i<N; i++ )
-  {
-     if(*m>a[i])
-     {
-         *m=a[i];
-     }
-  } 
-  return  i ;
+
+
+int buscarMenor(int arr[N], int *p)
+{
+  int i;
+  int menor=arr[0];
+  for (i=0; i<N ; i++ )
+  { 
+    if(arr[i]<menor)
+     menor=arr[i];
+     *p=i;
+  }
+ return menor;
 }
-*/
+
+
